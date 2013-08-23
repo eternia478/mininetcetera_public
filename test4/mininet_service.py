20c3b464fe1164def59c5d00e6e3ed794af76dfa
guoluyi <guoluyi1992@gmail.com>
2013-08-23 12:42:01 -0700

    cmsmsg.mininet_service: Implement cmd types
    use cmd to process different kinds of msg differently

--- test4/mininet_service.py
+++ test4/mininet_service.py
@@ -6,22 +6,43 @@ This file could work with pox_mininet.py to receive msg sent from that file,, it
 from pox.core import core
 from pox.messenger import *
 from pox.lib.revent.revent import autoBindEvents
+from pox.core import core as core
+
+log = core.getLogger()
 
 class CMSBot (ChannelBot):
-  def _unhandled (self, event):
-    print "4" 
-    # print "CMBot msg: ", event.msg
+  def _exec_cmd_migrated (self, event):
     if event.msg.get("CHANNEL",'CMS'):
+      print "migrated"
+      print "CMBot msg: ", event.msg
       msglevel = event.msg.get("msglevel")
-      msgtype = event.msg.get("type")
-      if msgtype == msglevel or msglevel == None:
-        print "CMBot msg: ", event.msg
-        host = event.msg.get("host") 
-        print "host", host
+      msgcmd = event.msg.get("cmd")
+      if msgcmd == msglevel or msglevel == None:
+        log.info("migrated")
+  
+  def _exec_cmd_instantiated (self, event):
+    if event.msg.get("CHANNEL",'CMS'):
+      print "instantiated"
+      print "CMBot msg: ", event.msg
+      msglevel = event.msg.get("msglevel")
+      msgcmd = event.msg.get("cmd")
+      if msgcmd == msglevel or msglevel == None:
+        log.info("instantiated")  
+  
+  def _exec_cmd_destroyed (self, event):
+    if event.msg.get("CHANNEL",'CMS'):
+      print "destroyed" 
+      print "CMBot msg: ", event.msg
+      msglevel = event.msg.get("msglevel")
+      msgcmd = event.msg.get("cmd")
+      if msgcmd == msglevel or msglevel == None:
+        log.info("destroyed")  
+  
+  
+  def _unhandled (self, event):
+    if event.msg.get("CHANNEL",'CMS'):
+      print "CMBot msg: ", event.msg 
         
-    # TODO: Here we could get the useful information and do what we need do here
-    # host = event.msg.get("host") 
-    # print "host", host
 
 def launch (nexus = "MessengerNexus"):
   def start (nexus):
