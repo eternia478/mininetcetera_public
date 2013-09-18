b69751102f474de9bb3792822fb7b0db1ed9464c
Murphy McCauley <murphy.mccauley@gmail.com>
2013-09-17 21:24:13 -0700

    Add PlainSwitch and POXNGSwitch

--- cmsnet/mininet_node_patch.py
+++ cmsnet/mininet_node_patch.py
@@ -22,8 +22,205 @@ from mininet.moduledeps import moduleDeps, pathCheck, OVS_KMOD, OF_KMOD, TUN
 from mininet.link import Link, Intf, TCIntf
 from mininet.node import Node, Host, Switch
 
+SWITCH_CONFIG_DIR = os.path.join(os.environ['CONFIG_DIR'], "swconf")
+if not os.path.isdir(SWITCH_CONFIG_DIR):
+  os.mkdir(SWITCH_CONFIG_DIR)
+
+class PlainSwitch ( Switch ):
+    def attach( self, intf ):
+        print self, "attach", intf
+
+    def detach( self, intf ):
+        print self, "detach", intf
+
+    def start( self, controllers ):
+        print self, "start", controllers
+        self.cmd("cd", SWITCH_CONFIG_DIR)
+
+    def stop( self ):
+        print self, "stop"
+
+    def execute():
+        print self, "execute"
 
 
+
+class POXNGSwitch( Switch ):
+    "Switch to run a POX application."
+
+    def __init__( self, name,
+                  address="127.0.0.1", port=6644, ports="", dpid=None,
+                  extra="", listenPort=None, **params):
+        """Init.
+           name: name for switch
+           control_flag: spawn personal controller?
+           control_type: personal controller type
+           address: IP address to listen to (and personal controller address)
+           port: TCP port to listen to (and personal controller port)
+           ports: network interfaces to connect
+           dpid: dpid for switch (or None to derive from name, e.g. s1 -> 1)
+           max_retry_delay: max time between retries to connect to controller
+           extra: extra parameters
+           listenPort: port to listen on for dpctl connections
+           params: Node parameters (see config() for details)"""
+        Switch.__init__(self, name, dpid, listenPort=listenPort, **params)
+
+        assert self.name.endswith(self.real_dpid)
+
+        if 'POX_CORE_DIR' not in os.environ:
+            self.poxCoreDir = os.path.join(os.path.expanduser("~"), "pox")
+            warn("POX_CORE_DIR is not set; using " + self.poxCoreDir)
+        else:
+            self.poxCoreDir = os.environ[ 'POX_CORE_DIR' ]
+        if not os.path.isdir(self.poxCoreDir):
+            exit( 'exiting; please set missing POX_CORE_DIR env var' )
+
+        self.controller_ip = address if address else "127.0.0.1"
+        self.controller_port = int(port) if port else 6633
+        self.input_intf_ports = ports.split(";") if ports else []
+        #self.max_retry_delay = max_retry_delay if max_retry_delay else 16
+        self.extra = format_pox_extra(extra).split(";") if extra else []
+
+        #TODO: use self.extra and self.max_retry_delay
+
+        # NOTE: In case these are needed elsewhere...
+        self.command = ""
+        self.intf_ports = []
+        self.pox_pid = None
+        self.started_switch = False
+
+        #for attr in type(self).__dict__:
+        #  if not callable(getattr(self, attr)): continue
+        #  def makeFoo (name, f):
+        #    def foo (*args, **kw):
+        #      print "CALL",name
+        #      return f(*args, **kw)
+        #  setattr(self, attr, makeFoo(attr, getattr(self, attr)))
+
+    @property
+    def real_dpid (self):
+        return str(int(self.dpid, 16))
+
+    def _build_cmd_args( self ):
+        "Build command-line argument of POX."
+
+        cmd = self.poxCoreDir + "/pox.py"
+        pathCheck( cmd )
+
+        args = []
+        args.append(cmd)
+
+        args.append("--no-openflow")
+
+        args.append("log.level --DEBUG")
+
+        logdir = "/tmp/log"
+        if not os.path.isdir(logdir):
+          os.mkdir(logdir)
+        logfile = os.path.join(logdir, "%s.log" % (self.name,))
+        args.append("log --file=%s,w" % logfile)
+
+        args.append("ng.dp")
+        args.append("--address=%s" % self.controller_ip)
+        args.append("--port=%s" % self.controller_port)
+        args.append("--dpid=%s" % self.real_dpid)
+
+        args.append("ng.dp.netdevs")
+        args.append('--devices="s%s-*"' % self.real_dpid)
+        #OR use self.intf_ports
+
+        self.command = " ".join(args)
+        #if self.print_personal_debug: print "EVAL: " + self.command
+
+    def _run_pox_switch( self ):
+        "Run the POX switch"
+        if self.pox_pid is not None:
+            warn( "Killing old pox switch to restart new one." )
+            self._kill_pox_switch()
+        self._build_cmd_args()
+        self.cmd( self.command + " &", printPid=True )
+        self.pox_pid = self.lastPid
+        self.started_switch = True
+
+    def _kill_pox_switch( self ):
+        "Kill the POX switch"
+        if self.pox_pid is None:
+            error( "No pox switch process to kill" )
+            return
+        #if self.print_personal_debug: print "KILL: process %d" % self.pox_pid
+        self.cmd( 'kill %d' % self.pox_pid )
+        self.pox_pid = None
+        self.started_switch = False
+
+    def attach( self, intf ):
+        "Connect a data port"
+        self.cmd( 'ifconfig', intf, '0.0.0.0 up' )
+        #intf_name = str(intf)
+        #
+        #if self.input_intf_ports:     # Only add specified ports.
+        #    if intf_name in self.input_intf_ports:
+        #        self.intf_ports.append(intf_name)
+        #else:                         # Add all ()-eth# ports.
+        #    if self.name in intf_name:
+        #        self.intf_ports.append(intf_name)
+        #if self.print_personal_debug:
+        #    print "intf_ports currently: "+str(self.intf_ports)
+        #
+        ## If already started, we need to restart the switch.
+        #if self.started_switch:
+        #    self._run_pox_switch()
+
+    def detach( self, intf ):
+        "Disconnect a data port"
+        self.cmd( 'ifconfig', intf, 'down' )
+        #intf_name = str(intf)
+        #if intf_name in self.intf_ports:
+        #    self.intf_ports.remove(intf_name)
+        #if self.print_personal_debug:
+        #    print "intf_ports currently: "+str(self.intf_ports)
+        #
+        ## If already started, we need to restart the switch.
+        #if self.started_switch:
+        #    self._run_pox_switch()
+
+    def start( self, controllers ):
+        if self.poxCoreDir is not None:
+            self.cmd( 'cd ' + self.poxCoreDir )
+
+        # We should probably call config instead, but this
+        # requires some rethinking...
+        self.cmd( 'ifconfig lo up' )
+        # Annoyingly, --if-exists option seems not to work
+        for intf in self.intfList(): #nameToIntf #self.intfNames
+            if not intf.IP():
+                self.attach( intf )
+
+        assert len(controllers) == 1
+        c = controllers[0]
+        self.controller_ip = c.IP()
+        self.controller_port = c.port
+
+        ## Add controllers.
+        ## NOTE: This case is currently impossible and inaccessible.
+        #if not self.controller_ip or not self.controller_port:
+        #    warn( 'warning: bad input controller ip and port' )
+        #    if len(controllers) == 1:
+        #        c = controllers[0]
+        #        self.controller_ip = c.IP()
+        #        self.controller_port = c.port
+        #    else:
+        #        raise Exception('Cannot find unique controller to connect to')
+
+        self._run_pox_switch()
+
+    def stop( self ):
+        "Stop controller."
+        self._kill_pox_switch()
+        self.terminate()
+
+    def execute():
+        # CHECK: Do we need this?
+        self.start([])
 
 
 
